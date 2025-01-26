98ff84ad601f6c161182e77c89d753b6a1f9ee82
esteep <esteep@Steepest.local>
2025-01-26 10:12:53 -0800

    Change to default export for Auth0 provider

--- src/auth/auth0-provider-with-config.jsx
+++ src/auth/auth0-provider-with-config.jsx
@@ -1,4 +1,7 @@
-export const Auth0ProviderWithConfig = ({ children }) => {
+// src/auth/auth0-provider-with-config.jsx
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const Auth0ProviderWithConfig = ({ children }) => {
   const domain = 'dev-5giozvplijcqa2pc.us.auth0.com';
   const clientId = 'hjqwcbJXC0HFUSiFBujw5SyGt8Y3Q8dY';
   const redirectUri = window.location.origin;
@@ -19,3 +22,5 @@ export const Auth0ProviderWithConfig = ({ children }) => {
     </Auth0Provider>
   );
 };
+
+export default Auth0ProviderWithConfig;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Auth0ProviderWithConfig } from './auth/auth0-provider-with-config'
+import { Auth0ProviderWithConfig } from './auth/auth0-provider-with-config.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

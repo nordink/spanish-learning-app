b892987f0cda4e71e4504f119d2c8578499994da
esteep <esteep@Steepest.local>
2025-01-27 15:31:56 -0800

    Switch to redirect-only authentication

--- src/auth/auth0-provider-with-config.jsx
+++ src/auth/auth0-provider-with-config.jsx
@@ -3,7 +3,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const Auth0ProviderWithConfig = ({ children }) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
   return (
@@ -11,7 +10,7 @@ const Auth0ProviderWithConfig = ({ children }) => {
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-        redirect_uri: redirectUri,
+        redirect_uri: `${window.location.origin}/callback`,
         audience: audience,
         scope: "openid profile email"
       }}

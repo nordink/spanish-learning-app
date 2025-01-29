6a83193d1803417c5cee7f8cc51d30ea819f6a54
esteep <esteep@Steepest.local>
2025-01-28 21:06:49 -0800

    Fix file casing for consistency

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './app.jsx';
+import App from './App.jsx';
 import './index.css';
 import { handleAuthentication } from './auth/auth0-client';
 
